994ef123fc263142f29121717e4ea7cdad779bc9
YanshikRajput <yanshikrajput99@gmail.com>
2021-03-31 18:50:08 +0530

    Angular Training

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TshirtdatabindingComponent } from './tshirtdatabinding/tshirtdatabinding.component';
+import { HelloComponent } from './hello/hello.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TshirtdatabindingComponent,
+    HelloComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
